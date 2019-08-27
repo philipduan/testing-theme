40294fbc5bcf6fdc844c1070719c26676ffd681e
philipAdvesa <philip.du@advesa.com>
2019-08-27 12:10:24 -0700

    Updates

---
+++ precache-manifest.d78e550aec66e18084f8bdae025b5621.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a63663d61b49af6433edabd5ef7ee0",
+    "url": "/testing-theme/index.html"
+  },
+  {
+    "revision": "895a125d5e896f123d08",
+    "url": "/testing-theme/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "52620474a536bd1b8369",
+    "url": "/testing-theme/static/css/main.b0dc4f18.chunk.css"
+  },
+  {
+    "revision": "895a125d5e896f123d08",
+    "url": "/testing-theme/static/js/2.08996ba6.chunk.js"
+  },
+  {
+    "revision": "52620474a536bd1b8369",
+    "url": "/testing-theme/static/js/main.a89dd796.chunk.js"
+  },
+  {
+    "revision": "3a3d7cf0310ec7988fe3",
+    "url": "/testing-theme/static/js/runtime~main.7169b487.js"
+  },
+  {
+    "revision": "7a6bc10028a79f78e424a9c71499fa77",
+    "url": "/testing-theme/static/media/avatar.7a6bc100.jpg"
+  },
+  {
+    "revision": "4d2f2f4baa617ba08685b4d9118dc029",
+    "url": "/testing-theme/static/media/bg-pricing.4d2f2f4b.jpeg"
+  },
+  {
+    "revision": "6ee66b2cb8a3d9988e37b83b3b4a590e",
+    "url": "/testing-theme/static/media/card-1.6ee66b2c.jpeg"
+  },
+  {
+    "revision": "ae10829f019b0103525a6cd1102ee213",
+    "url": "/testing-theme/static/media/card-2.ae10829f.jpeg"
+  },
+  {
+    "revision": "4ae4eeff542ab32204e8409d170e14a0",
+    "url": "/testing-theme/static/media/card-3.4ae4eeff.jpeg"
+  },
+  {
+    "revision": "3122abf4a3e1067926c08dee7684522d",
+    "url": "/testing-theme/static/media/card-profile1-square.3122abf4.jpg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/testing-theme/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "ebe9884b2152a08a525fcd5054019532",
+    "url": "/testing-theme/static/media/image_placeholder.ebe9884b.jpg"
+  },
+  {
+    "revision": "35f7d126c07a40a893f3f6cc8d1c3717",
+    "url": "/testing-theme/static/media/lock.35f7d126.jpeg"
+  },
+  {
+    "revision": "59fc83e26c663ade5dcae6e7d56ca970",
+    "url": "/testing-theme/static/media/login.59fc83e2.jpeg"
+  },
+  {
+    "revision": "eec7c7f60134e712ef7174c96ca7ee5a",
+    "url": "/testing-theme/static/media/logo-white.eec7c7f6.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing-theme/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aba54d652e5df3b9f1f8024cbeb6630f",
+    "url": "/testing-theme/static/media/marc.aba54d65.jpg"
+  },
+  {
+    "revision": "1eb7fff2a469da3cc6c3311572d7696d",
+    "url": "/testing-theme/static/media/placeholder.1eb7fff2.jpg"
+  },
+  {
+    "revision": "081bf019322fc100b3b84f34d9bff302",
+    "url": "/testing-theme/static/media/product1.081bf019.jpg"
+  },
+  {
+    "revision": "a0e400118b382fc007111de984da7552",
+    "url": "/testing-theme/static/media/product3.a0e40011.jpg"
+  },
+  {
+    "revision": "49b54380ba092ba68ab160f3dd8d5773",
+    "url": "/testing-theme/static/media/register.49b54380.jpeg"
+  },
+  {
+    "revision": "61c4c50e61d68de31c8951af5470a2c2",
+    "url": "/testing-theme/static/media/sidebar-1.61c4c50e.jpg"
+  },
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/testing-theme/static/media/sidebar-2.d30c9e30.jpg"
+  },
+  {
+    "revision": "c963330bcd48670a75ffdd38622b1532",
+    "url": "/testing-theme/static/media/sidebar-3.c963330b.jpg"
+  },
+  {
+    "revision": "f34e943fbffb73c68e761828b1c00980",
+    "url": "/testing-theme/static/media/sidebar-4.f34e943f.jpg"
+  }
+]);
